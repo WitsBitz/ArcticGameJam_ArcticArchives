d7c73755a4f9a10691af5772e4c2e71825ff3acd
WitsBitz <roldan@thecosmicwhale.com>
2017-05-03 20:31:29 -0600

    push current project, initialize git

---
+++ Assets/LoadNextScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour {
+
+	void Start () {
+		Invoke("LoadScene", 20);
+	}
+	
+	void Update () {
+		
+	}
+
+	void LoadScene(){
+		SceneManager.LoadScene("main");
+	}
+}
---
+++ Assets/PlayInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayInfo : MonoBehaviour {
+
+	private AudioSource audioSource;
+	private GameObject player;
+	bool hasPlayed = false;
+
+	void Awake () {
+		audioSource = GetComponent<AudioSource>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		Debug.Log(Vector3.Distance(this.transform.position, player.transform.position));
+	}
+
+	void Update(){
+		if(Vector3.Distance(this.transform.position, player.transform.position) < 10 && hasPlayed == false)
+		{
+			audioSource.Play();
+			hasPlayed = true;
+		}
+	}
+}
